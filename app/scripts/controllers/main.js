b695afbff9b9a058fa97e6c399cee17e60470677
Quentin Bahers <quentin.bahers@gmail.com>
2014-07-20 23:20:17 +0200

    Step 7: Use Bower to install packages

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,7 +9,7 @@
  */
 angular.module('mytodoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.todos = ['Item 1', 'Items 2', 'Item 3'];
+    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
     $scope.addTodo = function () {
       $scope.todos.push($scope.todo);
       $scope.todo = '';
